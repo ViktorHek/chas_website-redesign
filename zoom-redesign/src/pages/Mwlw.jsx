db14e79304a3e89e8b8b781bf144cda8420648ad
viktorHek <viktorkarlsson125@gmail.com>
2023-02-06 17:27:38 +0100

    change index placement

--- zoom-redesign/src/pages/Mwlw.jsx
+++ zoom-redesign/src/pages/Mwlw.jsx
@@ -6,7 +6,8 @@ function Mwlw() {
       <div className="mwlw-img-container center">
         <img
           src="https://st2.zoom.us/static/6.3.11147/image/home2/zm-product-wheel.png"
-          alt="img" style={{maxHeight: '200%'}}
+          alt="img"
+          style={{ maxHeight: "200%" }}
         />
       </div>
       <div className="mwlw-text-container">
@@ -16,7 +17,9 @@ function Mwlw() {
           Simple to manage and delightful to use, Zoom powers the modern
           workforce.
         </span>
-        <a className="mwlw-link" href="google.com">Discover the Possibilities <span></span></a>
+        <a className="mwlw-link" href="google.com">
+          Discover the Possibilities
+        </a>
       </div>
     </div>
   );
